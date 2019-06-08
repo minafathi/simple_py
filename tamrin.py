6eba219ff3349c9a47a3e24bd347203fe7fa56de
Mina Fathi <mina.fathi2012@gmail.com>
2019-06-08 06:47:27 -0700

    Add files via upload

---
+++ tamrin.py
@@ -0,0 +1,16 @@
+my_number = 10
+print(f"my number is {my_number}")
+if my_number % 2 == 0:
+    message = "It's Even"
+else:
+     message = "It's Odd"
+print(message)
+
+my_number += 1
+print(f"my number plus 1 is {my_number}")
+
+if my_number % 2 == 0:
+    message2 = "Now it became Even"
+else:
+     message2 = "Now it became Odd"
+print(message2)
